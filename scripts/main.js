51f73be6e5ffede635fcda1c19c5d553276f491e
Pavel Dobiáš <velepaveld9@gmail.com>
2022-02-26 12:58:09 +0100

    Timer now resets correctly when player dies.

--- scripts/main.js
+++ scripts/main.js
@@ -14,18 +14,25 @@ function tickEvent (eventData) {
         for (const player of world.getPlayers()) {
             if (Object.keys(playerMovement).includes(player.name)) {
                 const velocity = player.velocity.x + player.velocity.z;
+                //Not moving:
                 if ((-0.001 < velocity) && (velocity < 0.001) || playerMovement[player.name] < 0) {
                     if (playerMovement[player.name] === timer && eventData.currentTick % 20 === 0) {
                         try {
                             player.runCommand('damage @s 2');
                         } catch {
-                            
+
                         }
                     }
                     playerMovement[player.name] = Math.min(timer, playerMovement[player.name] + randInt(2,10));
+                //Moving:
                 } else if (playerMovement[player.name] >= 0) {
                     playerMovement[player.name] = Math.max(0, playerMovement[player.name] - randInt(1,5));
                 }
+                //Check if dead:
+                if (player.getComponent('minecraft:health').current === 0) {
+                    playerMovement[player.name] = 0;
+                }
+            //Check if player just loaded:
             } else {
                 playerMovement[player.name] = -275;
             }
