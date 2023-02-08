505d3af95eef3e31fc4e011aacc3ba81b56973ab
Pavel Dobiáš <pavel23dob@gmail.com>
2023-02-08 10:51:19 +0100

    Version 1.5 - 1.19.60 Patch

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,38 @@
 import {world,system} from "@minecraft/server";
 import {timer} from "./modules/addon_options.js";
-var playerMovement = {};
+const playerMovement = {};
 
 function randInt(min, max) {
-      max++;
-      return Math.floor(Math.random() * (max - min)) + min;
+      return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-async function tickEvent(eventData) {
-    system.run(tickEvent);
-    if (eventData.currentTick % 5 === 0) {
-        for (const player of world.getPlayers()) {
-            if (player.id in playerMovement) {
-                const velocity = player.velocity.x + player.velocity.z;
-                if ((-0.001 < velocity) && (velocity < 0.001) || playerMovement[player.id] < 0) {
-                    if (playerMovement[player.id] === timer && eventData.currentTick % 20 === 0) {
-                        try {
-                            await player.runCommandAsync('damage @s 2');
-                        } catch {}
-                    }
-                    playerMovement[player.id] = Math.min(timer, playerMovement[player.id] + randInt(2,10));
-                } else if (playerMovement[player.id] >= 0) {
-                    playerMovement[player.id] = Math.max(0, playerMovement[player.id] - randInt(1,5));
-                }
-                if (player.getComponent('minecraft:health').current === 0) {
-                    playerMovement[player.id] = -20;
-                }
-            } else {
-                playerMovement[player.id] = -275;
-            }
-        }
+//Calculate movement:
+system.runSchedule(playerMovementTick,5);
+
+//Deal Damage:
+system.runSchedule(playerDamageTick,20);
+
+function playerDamageTick() {
+    for (const player of world.getPlayers()) {
+        console.warn(`${player.name} [${playerMovement[player.id]}] - Damage`);
+        if (playerMovement[player.id] === timer) player.applyDamage(2);
     }
 }
 
-system.run(tickEvent);
+function playerMovementTick() {
+    for (const player of world.getPlayers()) {
+        if (player.id in playerMovement) {
+            const velocity = player.velocity.x + player.velocity.z;
+            if ((-0.001 < velocity) && (velocity < 0.001) || playerMovement[player.id] < 0) {
+                playerMovement[player.id] = Math.min(timer, playerMovement[player.id] + randInt(2,10));
+            } else if (playerMovement[player.id] >= 0) {
+                playerMovement[player.id] = Math.max(0, playerMovement[player.id] - randInt(1,5));
+            }
+            if (player.getComponent('minecraft:health').current === 0) {
+                playerMovement[player.id] = -45;
+            }
+        } else {
+            playerMovement[player.id] = -275;
+        }
+    }
+}
