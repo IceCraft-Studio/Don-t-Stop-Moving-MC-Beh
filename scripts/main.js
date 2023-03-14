f53cfb628b06672dc85a1a0d72e5b306c9418b81
Pavel Dobiáš <pavel23dob@gmail.com>
2023-03-14 21:23:45 +0100

    Fixed breaking changes of Script-API in version 1.19.70.02

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,27 @@
-import {world,system} from "@minecraft/server";
-import {timer} from "./modules/addon_options.js";
-const playerMovement = {};
+import { world } from "@minecraft/server";
+import { mobList } from "./modules/mob_list.js";
+
+world.events.blockBreak.subscribe(async (eventData) => {
+    const locX = eventData.block.location.x;
+    const locY = eventData.block.location.y;
+    const locZ = eventData.block.location.z;
+    await eventData.player.runCommandAsync(`summon ${randChoice(mobList)} ${locX} ${locY} ${locZ}`);
+});
 
 function randInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+    max++;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-//Calculate movement:
-system.runSchedule(playerMovementTick,5);
-
-//Deal Damage:
-system.runSchedule(playerDamageTick,20);
-
-function playerDamageTick() {
-    for (const player of world.getPlayers()) {
-        console.warn(`${player.name} [${playerMovement[player.id]}] - Damage`);
-        if (playerMovement[player.id] === timer) player.applyDamage(2);
-    }
-}
-
-function playerMovementTick() {
-    for (const player of world.getPlayers()) {
-        if (player.id in playerMovement) {
-            const velocity = player.velocity.x + player.velocity.z;
-            if ((-0.001 < velocity) && (velocity < 0.001) || playerMovement[player.id] < 0) {
-                playerMovement[player.id] = Math.min(timer, playerMovement[player.id] + randInt(2,10));
-            } else if (playerMovement[player.id] >= 0) {
-                playerMovement[player.id] = Math.max(0, playerMovement[player.id] - randInt(1,5));
-            }
-            if (player.getComponent('minecraft:health').current === 0) {
-                playerMovement[player.id] = -45;
-            }
-        } else {
-            playerMovement[player.id] = -275;
+function randChoice(array, num) {
+    if (num) {
+        const choice = [];
+        while (num > 0) {
+            num--;
+            choice.push(array[randInt(0, array.length - 1)]);
         }
+        return choice;
+    } else {
+        return array[randInt(0, array.length - 1)];
     }
 }
